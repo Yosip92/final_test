654d1a6d3f4a110baac47f7c298bae9d1686aafc
Dmitro Raday <yosip92@gmail.com>
2017-05-03 20:12:45 +0300

    worked js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 console.log('HELO JS');
 
-$(function () {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
-})
+var page = location.hash;
+console.log('page',page);
+function Style(page){
+  if (page == "#home"){
+    document.getElementById("home2").style = "background: black;"
+  }
+  else if (page == "#blog"){
+    document.getElementById("blog2").style = "background: black;"
+  }
+}
