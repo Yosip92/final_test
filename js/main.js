11235aebdff00b4eef68c9b0c5d3ee261c5664ef
Yosip92 <yosip92@gmail.com>
2017-05-07 19:36:20 +0300

    fixsed again

--- js/main.js
+++ js/main.js
@@ -1,12 +1,46 @@
-console.log('HELO JS');
+// console.log('HELO JS');
 
-var page = location.hash;
-console.log('page',page);
-function Style(page){
-  if (page == "#home"){
-    document.getElementById("home2").style = "background: black;"
-  }
-  else if (page == "#blog"){
-    document.getElementById("blog2").style = "background: black;"
-  }
+
+function myClick(url) {
+//   console.log("url", url);
+//     var newUrl = location.host + location.pathname;
+//   console.log("newUrl", newUrl);
+// var newUrl = location.host + location.pathname;
+// console.log("newUrl", newUrl);
+// var response = document.getElementById("menu").innerHTML;
+// window.history.pushState(null, null, newUrl);
+
+     $(".menuItem").css("background","#87509c");
+    switch (url) {
+      case "/home1": {
+        $(".home1").css("background","#643a79");
+        location = location.pathname + "/home";
+        break;
+      }
+      case "/blog1": {
+        $(".blog1").css("background","#643a79");
+        break;
+      }
+      case "/work1": {
+        $(".work1").css("background","#643a79");
+        break;
+      }
+      case "/about1": {
+        $(".about1").css("background","#643a79");
+        break;
+      }
+      case "/contact1": {
+        $(".contact1").css("background","#643a79");
+        break;
+      }
+    }
+}
+
+el = document.getElementById("menu");
+el.addEventListener("click", function(url){myClick}, false);
+
+// window.addEventListener(hashchange)
+function change(){
+  var url = location.hash;
+  replace(url[0])
 }
